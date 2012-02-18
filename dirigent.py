ef98e22100f31f9b546728ad4c9f2ffc5ecd829b
waawal <daniel.waardal@vodial.com>
2012-02-18 22:37:39 +0100

    with-statements now incorporates args and kwargs + some minor docstring additions

--- dirigent.py
+++ dirigent.py
@@ -5,36 +5,38 @@ __all__ = "notify", "observe"
 
 
 class ObserverClass(object):
-
-
+    """ Object holding all the observers, aliased to notify.
+    """
     def __init__(self):
         self.observers = defaultdict(list)
         
     def register(self, name, func):
-        ''' Register a callback for a event
-        '''
+        """ Register a callback for a event
+        """
         self.observers[name].append(func)
 
     def unregister(self, name, func):
-        ''' Unregisters a event. '''
+        """ Unregisters a event.
+        """
         if name in self.observers and func in self.observers[name]:
             self.observers[name].remove(func)
 
-    def notify(self, name, *arg, **kw):
-        ''' Notifies all registered observers of a registered event.
+    def notify(self, name, *args, **kwargs):
+        """ Notifies all registered observers of a registered event.
             Returns a list of tuples (called object, result)
-        '''
+        """
         Result = namedtuple('Result', 'called result')
 
-        return [Result(observer, observer(*arg, **kw)) for observer
+        return [Result(observer, observer(*args, **kwargs)) for observer
                 in self.observers[name]]
 
     __call__ = notify
 
 
 class NotificationClass(object):
-   # TODO: Should support *args and *kwargs
-   def __init__(self, name, before=None, after=None):
+   """ Implements the with-statement.
+   """
+   def __init__(self, name, before=None, after=None, *args, **kwargs):
        self.name = name
        if before:
            self.before = True
@@ -42,14 +44,17 @@ class NotificationClass(object):
            self.after = True
        if not before and not after:
            self.before = True
+           
+       self.args = args
+       self.kwargs = kwargs
 
    def __enter__(self):
        if self.before:
-           notify(self.name)
+           notify(self.name, *self.args, **self.kwargs)
 
    def __exit__(self, exctype, excvalue, traceback):
        if not exctype and not excvalue and not traceback and self.after:
-           notify(self.name) 
+           notify(self.name, *self.args, **self.kwargs) 
 
 
 def observe(name):
