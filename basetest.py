16ca3c4db0c6782375b79ec154a146176249de5a
Daniel W <waawal@boom.ws>
2012-03-29 21:43:43 +0400

    tidy up work

--- basetest.py
+++ basetest.py
@@ -1,38 +1,35 @@
-from dirigent import TornadoSubject
+from dirigent import subject
 import random
 import time
 
-events = TornadoSubject()
+events = subject()
 
 @events.register
 def a(*args, **kwargs):
-    print "a:", args, kwargs
+    print "a:", args[0], kwargs
     time.sleep(random.randint(0,2))
     print "returned: a", args[0] 
 
 @events.register
 def b(*args, **kwargs):
-    print "b:", args, kwargs
+    print "b:", args[0], kwargs
     time.sleep(random.randint(0,2))
     print "returned: b", args[0] 
 
 @events.register
 def c(*args, **kwargs):
-    print "c:", args, kwargs
+    print "c:", args[0], kwargs
     time.sleep(random.randint(0,2))
     print "returned: c", args[0] 
 
 @events.register
 def d(*args, **kwargs):
-    print "d:", args, kwargs
+    print "d:", args[0], kwargs
     time.sleep(random.randint(0,2))
     print "returned: d", args[0]
 
 
-for i, _ in enumerate(xrange(5)):
+for i, _ in enumerate(xrange(2)):
     events.notify(i, hello="world!")
     print "Moving on..."
-    time.sleep(1)
-print "waiting..."
-time.sleep(3)
 print "done!"
