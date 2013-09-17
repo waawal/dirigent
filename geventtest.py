c3e72528ab1906e214c6a9fd91a77873cd84c085
Daniel Waardal <waawal@boom.ws>
2013-09-17 16:55:22 +0200

    imports

--- geventtest.py
+++ geventtest.py
@@ -1,4 +1,4 @@
-from dirigent.backends.gevent import Subject
+from dirigent.backends.geventbackend import Subject
 import gevent
 import random
 
@@ -9,29 +9,33 @@ events = Subject()
 def a(*args, **kwargs):
     print "a:", args, kwargs
     gevent.sleep(random.randint(0,2))
-    print "returned: a", args[0] 
+    print "returned: a", args[0]
+    return True 
 
 @events.register
 def b(*args, **kwargs):
     print "b:", args, kwargs
     gevent.sleep(random.randint(0,2))
-    print "returned: b", args[0] 
+    print "returned: b", args[0]
+    return True 
 
 @events.register
 def c(*args, **kwargs):
     print "c:", args, kwargs
     gevent.sleep(random.randint(0,2))
-    print "returned: c", args[0] 
+    print "returned: c", args[0]
+    return True 
 
 @events.register
 def d(*args, **kwargs):
     print "d:", args, kwargs
     gevent.sleep(random.randint(0,2))
     print "returned: d", args[0]
+    return True
 
 
 for i, _ in enumerate(xrange(5)):
-    events.notify(i, hello="world!")
+    print events.notify(i, hello="world!")
     print "Moving on..."
     gevent.sleep(1)
 gevent.sleep(3)
